4709e17e8d3fe1047858d7b711215ad9b55999e5
Dima Prosto <csourgg@gmail.com>
2024-08-14 23:23:34 +0300

    add addTask

--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -9,15 +9,14 @@ const ToDo = () => {
   return (
     <section>
       <h2>Todo</h2>
-        <div>
-            <Formik initialValues={{content:''}} onSubmit={addTask}>
-                <Form>
-                    <Field name="content"/>
-
-                    <input type="submit" value='Add'/>
-                </Form>
-            </Formik>
-        </div>
+      <div>
+        <Formik initialValues={{ content: "" }} onSubmit={addTask}>
+          <Form>
+            <Field name="content" />
+            <input type="submit" value="Add" />
+          </Form>
+        </Formik>
+      </div>
       <div>
         <h3>list tasks</h3>
         <ol>
@@ -31,3 +30,6 @@ const ToDo = () => {
 };
 
 export default ToDo;
+
+//PFM2024-1_REACT_hooks_practice_083_2
+//https://www.youtube.com/watch?v=o4JYqtOkGs0&list=PLxQIdU5bMkOiUg3p6X4BXVpIfWzMaLV7l&index=180
